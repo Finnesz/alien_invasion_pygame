95053dc82c1e54bbac2b7f005f6a26ffca2493b3
--global <126937927+Finnesz@users.noreply.github.com>
2025-08-27 17:00:37 +0800

    Added dynamic settings for difficulty

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 import pygame
+from enums import Difficulty
+
 
 class Settings:
     """To hold all the values and settings for the game."""
@@ -21,22 +23,52 @@ class Settings:
         self.bg_color = self.palette[0]
 
         # Ship settings
-        self.ship_speed = 1.5
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_speed = 5.0
-        self.bullet_width = 3
+        self.bullet_width = 5
         self.bullet_height = 15
         self.bullet_color = self.palette[1]
         self.bullet_bg = self.palette[1]
         self.bullet_limit = 3
 
         # Font settings
-        self.font = pygame.font.SysFont("smallfontregular", 16)
+        self.font = pygame.font.SysFont("Noto Sans", 16)
         # print(pygame.font.get_fonts())
 
         # Alien settings
-        self.alien_speed = 1.0
         self.drop_down_speed = 10
-        self.fleet_direction = 1
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+        # How quickly the alien point values increase
+        self.score_scale = 1.5
+        self.set_difficulty = Difficulty.EASY # default is easy
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        match self.set_difficulty:
+            case Difficulty.EASY: 
+                self.ship_speed = 1.5
+                self.bullet_speed = 2.5
+                self.alien_speed = 1.0
+            case Difficulty.MEDIUM:
+                self.ship_speed = 2.1
+                self.bullet_speed = 3.1
+                self.alien_speed = 2.6
+            case Difficulty.HARD:
+                self.ship_speed = 2.4
+                self.bullet_speed = 3.4
+                self.alien_speed = 3.9
+
+        self.alien_points = 50
+        self.fleet_direction = 1 # fleet direction of 1 represents right; -1 represents left.
+        
+    def increase_speed(self):
+        """Increase speed settings."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
