2e766272214aa156fc9614f2a890d272d6f57e32
--global <126937927+Finnesz@users.noreply.github.com>
2025-08-27 16:58:50 +0800

    Added TButton for customizable bg

--- button.py
+++ button.py
@@ -2,17 +2,17 @@ import pygame
 from pygame.font import SysFont
 
 class Button:
-    def __init__(self, ai_game, msg):
+    def __init__(self, ai_game, msg, x, y):
         self.screen = ai_game.screen
         self.settings = ai_game.settings
-        self.screen_rect = self.screen.get_frect()
+        self.screen_rect = ai_game.screen_rect
 
         self.width, self.height = 200, 50
         self.color = self.settings.palette[3]
-        self.font = pygame.font.SysFont("smallfontregular", 48)
-        
+        self.font = pygame.font.SysFont("Noto Sans", 48)
+
         self.button_color = self.settings.palette[1]
-        self.rect = pygame.FRect(0, 0, self.width, self.height)
+        self.rect = pygame.FRect(x, y, self.width, self.height)
         self.rect.center = self.screen_rect.center
 
         self._prep_msg(msg)
@@ -24,7 +24,7 @@ class Button:
 
         self.font_img_rect.center = self.rect.center
         # self.font_img_rect.centery += (self.font_img_rect.height) - 1
-    
+
     def draw_button(self):
         """Draw button and then the text"""
         self.screen.fill(self.button_color, self.rect)
@@ -32,25 +32,32 @@ class Button:
 
 class TButton(Button):
     """Button with texture"""
-    def __init__(self, ai_game, msg, img_dir):
+    def __init__(self, ai_game, msg, img_dir, x, y):
         self.width, self.height = 200, 50
 
         self.texture_img = pygame.image.load(img_dir)
         self.texture_img = pygame.transform.scale(self.texture_img, (self.width, self.height))
         self.texture_rect = self.texture_img.get_frect()
-        self.texture_rect.center = ai_game.screen.get_frect().center
+        self.texture_rect.x = x
+        self.texture_rect.y = y
 
-        super().__init__(ai_game, msg) # had to put it here because Button class's _prep_msg does not recognize texture when initialized
+        super().__init__(ai_game, msg, x, y) # had to put it here because Button class's _prep_msg does not recognize texture when initialized
 
     def _prep_msg(self, msg):
         # print("TButton prep")
         self.font_img = self.font.render(msg, False, self.color)
         self.font_img_rect = self.font_img.get_frect()
-  
-        self.font_img_rect.center = self.texture_rect.center 
+
+        self.font_img_rect.center = self.texture_rect.center
         # self.font_img_rect.centery += (self.font_img_rect.height) - 1
-    
+
     def draw_button(self):
         """Draw button and then the text"""
         self.screen.blit(self.texture_img, self.texture_rect)
         self.screen.blit(self.font_img, self.font_img_rect)
+
+# TODO: Do this some other time lol
+class Arrow():
+    """Arrows to show which button is being hovered by the mouse."""
+    def __init__(self):
+        pass
