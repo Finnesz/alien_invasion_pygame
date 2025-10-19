44e2cbcd0fc7360dc17d448039551b54dd1cbd5e
--global <126937927+Finnesz@users.noreply.github.com>
2025-10-19 18:22:44 +0800

    added AlienBullet class

--- bullet.py
+++ bullet.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.sprite import Sprite
 
+
 class Bullet(Sprite):
     """Create a bullet object at the ship's current position."""
 
@@ -9,19 +10,34 @@ class Bullet(Sprite):
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.color = self.settings.bullet_color
-        self.bg_color = self.settings.bullet_bg
 
         # Create a bullet rect at (0, 0) and then set corrent position.
         self.rect = pygame.FRect(0, 0, self.settings.bullet_width + 4, self.settings.bullet_height + 4)
-        self.bg_rect = pygame.FRect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
         self.rect.midbottom = ai_game.ship.rect.midtop
         self.rect.x += 1
-        self.bg_rect.center = self.rect.center
 
     def update(self):
         self.rect.y -= self.settings.bullet_speed
-        self.bg_rect.y -= self.settings.bullet_speed
 
     def draw_bullet(self):
         pygame.draw.rect(self.screen, self.color, self.rect)
-        pygame.draw.rect(self.screen, self.bg_color, self.bg_rect)
+
+
+class AlienBullet(Sprite):
+    """Create a bullet object at an alien's current position."""
+
+    def __init__(self, ai_game, alien):
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.palette[1]
+
+        self.rect = pygame.FRect(0, 0, self.settings.bullet_width + 4, self.settings.bullet_height + 4)
+        self.rect.centerx = alien.rect.centerx
+        self.rect.top = alien.rect.bottom
+
+    def update(self):
+        self.rect.y += self.settings.bullet_speed
+
+    def draw_bullet(self):
+        pygame.draw.rect(self.screen, self.color, self.rect)
